102b34595ba07c910ea2ff276741c29ef5083609
ansh8tu <anshu8tu@gmail.com>
2020-03-26 22:10:40 +0530

    R171219006: Added Addition function

--- substract.cpp
+++ substract.cpp
@@ -2,16 +2,20 @@
 #include<cmaths>
 using namespace std;
 float substract(float x,float y);
+float add(float x, float y);
 int main()
 {
 float first_no;
 float second_no;
 float diff_of_nos;
+float sum_of_numbers;
 
 cout<<"enter the 2 numbers"<<endl;
 cin>>first_no>>second_no;
 diff_of_nos= substract(first_no, second_no);
+sum_of_numbers=add(number_1, number_2);
 cout<<"difference of two numbers"<<diff_of_nos<<endl;
+cout<<"sum of two numbers "<<sum_of_numbers<<endl;
 return 0;
 }
 float substract(float x,float y)
@@ -20,5 +24,10 @@ if(x>y)
 	return (x-y);
 if(y>x)
 	return (y-x);
+}
+
+float add(float x, float y)
+{
+    return (x+y);
 }
 
