f8900697ca9213f741ae83e06962036bc0f050e1
animesh-deka <animesh7637@gmail.com>
2020-03-27 19:10:59 +0530

    R171219005:Added modulo function

--- substract.cpp
+++ substract.cpp
@@ -3,19 +3,23 @@
 using namespace std;
 float substract(float x,float y);
 float multiply(float x, float y);
+float modulo(float x, float y);
 int main()
 {
 float first_no;
 float second_no;
 float diff_of_nos;
 float product_of_numbers;
+float remainder;
 
 cout<<"enter the 2 numbers"<<endl;
 cin>>first_no>>second_no;
 diff_of_nos= substract(first_no, second_no);
 product_of_numbers=multiply(first_no, second_no);
+remainder=modulo(first_no, second_no);
 cout<<"difference of two numbers"<<diff_of_nos<<endl;
 cout<<"product of two numbers"<<product_of_numbers<<endl;
+cout<<"remainder is"<<remainder<<endl;
 return 0;
 }
 float substract(float x,float y)
@@ -28,5 +32,13 @@ if(y>x)
 float multiply(float x, float y)
 {
 	return (x*y);
+}
+float modulo(float x, float y)
+{
+	if(second_no==0)
+		cout<<"INVALID";
+	else
+		return 0;
+
 }
 
