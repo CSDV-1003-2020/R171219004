7312685d6c00219ee7ff01d7ba1ae8fe5ce135fa
ansh8tu <anshu8tu@gmail.com>
2020-03-27 18:31:58 +0530

    R171219006: Added Division function

--- substract.cpp
+++ substract.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 float substract(float x,float y);
 float add(float x, float y);
+float divide(float x, float y);
 int main()
 {
 float first_no;
@@ -13,9 +14,11 @@ float sum_of_numbers;
 cout<<"enter the 2 numbers"<<endl;
 cin>>first_no>>second_no;
 diff_of_nos= substract(first_no, second_no);
-sum_of_numbers=add(number_1, number_2);
+sum_of_numbers=add(first_no, second_no);
+division_of_numbers=divide(first_no, second_no);
 cout<<"difference of two numbers"<<diff_of_nos<<endl;
 cout<<"sum of two numbers "<<sum_of_numbers<<endl;
+cout<<"division of two numbers "<<division_of_numbers<<endl;
 return 0;
 }
 float substract(float x,float y)
@@ -31,3 +34,15 @@ float add(float x, float y)
     return (x+y);
 }
 
+float divide(float x, float y)
+{
+	if(second_no==0)
+	{
+		cout<<"Invalid input for second number "<<endl;
+	}
+
+	else
+	{
+		return first_no/second_no;
+	}
+}
