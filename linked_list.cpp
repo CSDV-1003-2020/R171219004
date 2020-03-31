2b820630ac3eeea25b4dfe0534fbef7f9b6b2e3a
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2020-03-31 17:05:04 +0530

    R171219004: added function for displaying the elements  of linked list

--- linked_list.cpp
+++ linked_list.cpp
@@ -327,3 +327,23 @@ void single_llist::update()
     }
     cout<<"Node Updated"<<endl;
 } 
+/*
+ * Display Elements of a link list
+ */
+void single_llist::display()
+{
+    struct node *temp;
+    if (start == NULL)
+    {
+        cout<<"The List is Empty"<<endl;
+        return;
+    }
+    temp = start;
+    cout<<"Elements of list are: "<<endl;
+    while (temp != NULL)
+    {
+        cout<<temp->info<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
